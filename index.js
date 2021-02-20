653263d0999c1377cd74446944fc2fd60de80396
arm1xd <69009433+arm1xd@users.noreply.github.com>
2021-02-20 16:39:23 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+// var containerBlock =document.getElementById('container')
+
+// console.log(containerBlock)
+
+
+// var navBlock =document.getElementById('nav-main')
+
+// console.log(navBlock)
+
+
+// let jojo = document.querySelectorAll('li')
+
+// console.log(jojo)
+// var navBlock =document.getElementById('nav-main')
+
+// let navBlock =document.getElementsByTagName('li')
+
+// console.log(navBlock);
+
+// for (let index = 0; index < navBlock.length; index++) {
+//     navBlock[index].addEventListener('click' , function(){
+//         alert('toucheds')
+//     })
+// }
+
+
+
+// var input =document.querySelector('input[type="text"]')
+// var button =document.querySelector('input[type="button"]')
+// var result =document.querySelector('#result')
+
+// button.addEventListener('click', function(){
+//     let inputValue = input.value
+
+//     result.insertAdjacentHTML('afterend',`<p>${inputValue}</p>`)
+// })
+
+// let button =document.querySelector('#change')
+// let baseURL = 'file:///home/student-pc-3/Documents/mikhaylets/js+html/'
+// let file = document.querySelector('link[href]')
+
+// button.addEventListener('click', function(){
+//    if(file.href === baseURL +'light-theme.css'){
+//     file.href = baseURL +'dark-theme.css'
+//    }else if(file.href === baseURL +'dark-theme.css'){
+//        file.href = baseURL +'light-theme.css'
+//    }
+
+// })
+
+
+
+
+
+// let button =document.querySelector('#change')
+// let baseURL = 'file:///home/student-pc-3/Documents/mikhaylets/js+html/'
+// let file = document.querySelector('link[href]')
+
+// button.addEventListener('click', function(){
+//    if(file.href === baseURL +'light-theme.css'){
+//     file.href = baseURL +'dark-theme.css'
+//    }else if(file.href === baseURL +'dark-theme.css'){
+//        file.href = baseURL +'light-theme.css'
+//    }
+
+// })
+
+
+
+
+
